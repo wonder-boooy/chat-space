d71e3fdb0d7dc8bd5b916c5222afacb0fbb0dfd9
wonder-boooy <yuma.wada.1127@gmail.com>
2019-01-14 17:44:11 +0900

    Searching users without current_user
    userの検索対象からcurrent_userを除く

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
