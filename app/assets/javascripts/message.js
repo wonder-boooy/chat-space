7e71074b8531169dfecfccfd93f325093729289a
wonder-boooy <yuma.wada.1127@gmail.com>
2019-01-15 15:27:47 +0900

    Auto-updating messages
    メッセージの自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,8 @@
 $(function(){
   function buildHTML(message){
-    if(message.content){
-      var content = `<div class="lower-message__content">${message.content}</div>`
-    } else {
-      var content = ""
-    }
-    if(message.image_url != null){
-      var image = `<div class="lower-message__image"><img src="${message.image_url}"></div>`
-    } else {
-      var image = ""
-    }
-    var html = `<div class="message">
+    var content = message.content ? `<div class="lower-message__content">${message.content}</div>` : ""
+    var image = message.image_url != null ? `<div class="lower-message__image"><img src="${message.image_url}"></div>` : ""
+    var html = `<div class="message", data-message-id=${message.id}>
   <div class="upper-message">
     <div class="upper-message__user-name">
       ${message.user_name}
@@ -26,11 +18,12 @@ $(function(){
 </div>`
     return html;
   };
-
+  // Sendボタンで発火する非同期通信
   $("#sendBtn").submit(function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr("action");
+
     $.ajax({
       type: "post",
       url: url,
@@ -40,10 +33,12 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $(".messages").append(html);
-      $("#sendBtn")[0].reset();
-      $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, 500);
+      console.log(data);
+        var html = buildHTML(data);
+        $(".messages").append(html);
+        $("#sendBtn")[0].reset();
+        $(".input-form").val();
+        $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, 500);
     })
     .fail(function(){
       alert("メッセージ入力もしくは画像選択してください");
@@ -51,5 +46,31 @@ $(function(){
     .always(function(){
       $(".form__submit").removeAttr("disabled");
     })
-  })
+  });
+  // 自動更新処理
+  var interval = setInterval(function(){
+    if(window.location.href.match(/\/groups\/\d*\/messages/)){
+      $.ajax({
+        url: location.href,
+        dataType: "json"
+      })
+      .done(function(data){
+        var id = $(".message").last().data("messageId");
+        var new_id = data[data.length-1].id
+        if (new_id > id){
+          var insertHTML = '';
+          data.forEach(function(message){
+            insertHTML += buildHTML(message);
+          });
+          $(".messages").html(insertHTML);
+          $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, 500);
+        }
+      })
+      .fail(function(){
+        alert("自動更新に失敗しました")
+      })
+    } else {
+      clearInterval(interval)
+    }
+  }, 5000);
 })
