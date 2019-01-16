eabe8c8d8322059fc2cb529df830cc2e7a99ad99
wonder-boooy <yuma.wada.1127@gmail.com>
2019-01-16 12:45:37 +0900

    画像ファイル名の編集

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/image/287/PB220048.jpg")
+    image File.open("#{Rails.root}/public/uploads/message/image/287/PB220048.JPG")
     user
     group
   end
