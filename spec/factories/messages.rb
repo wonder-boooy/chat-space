3456ea2cbde5c101c73bd5a74600044540d95a51
wonder-boooy <yuma.wada.1127@gmail.com>
2019-01-16 12:26:59 +0900

    アセットファイルコンパイル前の修正
    FactoryGirlに定義したimageファイルを編集

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/image/343/PB220048.jpg")
+    image File.open("#{Rails.root}/public/uploads/message/image/287/PB220048.jpg")
     user
     group
   end
